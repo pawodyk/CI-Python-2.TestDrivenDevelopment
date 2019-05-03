42f165bcdef7cb25da8481dbb97b64eac719f5e3
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-03 17:47:50 +0100

    added function to pass test 4

--- evens.1.py
+++ evens.1.py
@@ -4,16 +4,19 @@ def even_number_of_evens(numbers):
     else:
         evens = 0
 
-        for item in numbers:
-            if item % 2 == 0:
-                evens += 1
-
+    for item in numbers:
+        if item % 2 == 0:
+            evens += 1
+    if evens == 0:
+        return False
+    else:
         return evens % 2 == 0
         
 
 assert even_number_of_evens([]) == False, "No number"
 assert even_number_of_evens([2,4]) == True, "Two even numbers"
 assert even_number_of_evens([2]) == False, "One even number"
+assert even_number_of_evens([1,3,9]) == False, "No even numbers"
 
 
 print("All test passed!!!")
