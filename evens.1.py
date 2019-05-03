afbeeace867ed98f0c7e531efcd646888498fce8
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-03 16:51:28 +0100

    Passed the third test"

--- evens.1.py
+++ evens.1.py
@@ -1,11 +1,19 @@
 def even_number_of_evens(numbers):
     if numbers == []:
         return False
-    else: 
-        return True
+    else:
+        evens = 0
+
+        for item in numbers:
+            if item % 2 == 0:
+                evens += 1
+
+        return evens % 2 == 0
+        
 
 assert even_number_of_evens([]) == False, "No number"
 assert even_number_of_evens([2,4]) == True, "Two even numbers"
+assert even_number_of_evens([2]) == False, "One even number"
 
 
 print("All test passed!!!")
