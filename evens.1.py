6866babd0ab6f1188694fe522c8ef5edc06dd132
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-03 18:13:15 +0100

    Code Refactoring - removed duplicate code checking for the empty array

--- evens.1.py
+++ evens.1.py
@@ -1,12 +1,11 @@
 def even_number_of_evens(numbers):
-    if numbers == []:
-        return False
-    else:
-        evens = 0
+    
+    evens = 0
 
     for item in numbers:
         if item % 2 == 0:
             evens += 1
+
     if evens == 0:
         return False
     else:
