ff1d308639332e1f5bd8afc8609834c8d38b5975
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-03 16:23:35 +0100

    completed the rest of the test

--- evens.py
+++ evens.py
@@ -13,9 +13,9 @@ def even_number_of_evens(numbers):
 assert even_number_of_evens([]) == False, "No number"
 assert even_number_of_evens([2]) == False, "One even number"
 assert even_number_of_evens([2,4]) == True, "two even numbers"
-# assert even_number_of_evens([2,3]) == False, "two numbers, one even"
-# assert even_number_of_evens([2,3,9,10,13,7,8]) == False, "Multiple numbers, three even"
-# assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "Multiple numbes, four even numbers"
-# assert even_number_of_evens([1,3,9]) == False, "No even numbers"
+assert even_number_of_evens([2,3]) == False, "two numbers, one even"
+assert even_number_of_evens([2,3,9,10,13,7,8]) == False, "Multiple numbers, three even"
+assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "Multiple numbes, four even numbers"
+assert even_number_of_evens([1,3,9]) == False, "No even numbers"
 
 print("All test passed!!!")
