9a87fd626d31965b2fccf2458b838197a70b2ca7
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-03 16:21:30 +0100

    added code to pass tests 1, 2 and 3

--- evens.py
+++ evens.py
@@ -1,9 +1,18 @@
 def even_number_of_evens(numbers):
-    return False
+    counter = 0
+
+    for item in numbers:
+        if item % 2 == 0:
+            counter += 1
+
+    if counter%2==0 and counter > 0:
+        return True
+    else:
+        return False
 
 assert even_number_of_evens([]) == False, "No number"
-# assert even_number_of_evens([2]) == False, "One even number"
-# assert even_number_of_evens([2,4]) == True, "two even numbers"
+assert even_number_of_evens([2]) == False, "One even number"
+assert even_number_of_evens([2,4]) == True, "two even numbers"
 # assert even_number_of_evens([2,3]) == False, "two numbers, one even"
 # assert even_number_of_evens([2,3,9,10,13,7,8]) == False, "Multiple numbers, three even"
 # assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "Multiple numbes, four even numbers"
