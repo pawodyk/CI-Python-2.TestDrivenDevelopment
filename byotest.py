189831af2e5e3e2ddf810cb32138fa996ec40f15
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-06 12:24:34 +0100

    extracted all unnecessary functions to the new file and retained just a testing framework functions

--- byotest.py
+++ byotest.py
@@ -1,4 +1,4 @@
-# Testing framework functions
+# Testing framework
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
@@ -20,36 +20,3 @@ def test_is_in(collection, item):
 def test_not_in(collection, item):
     assert item not in collection, "set {0} contains {1}".format(
         collection, item)
-
-
-# Test function:
-def number_of_evens(nums):
-    return sum([1 for num in nums if num % 2 == 0])
-
-
-numberSet = [1, 2, 3, 4, 5]
-
-# Testing correct execution of number_of_events function
-
-test_are_equal(number_of_evens(numberSet), 2)
-test_not_equal(number_of_evens(numberSet), 0)
-test_not_equal(number_of_evens(numberSet), 5)
-test_is_in(numberSet, 2)
-test_is_in(numberSet, 5)
-
-print("The number_of_evens function passed all tests!")
-
-
-# this tests are designed to fail, please uncomment the one you wish to execute
-
-print("\nNow, testing the testing framework")
-
-# test_are_equal(number_of_evens(numberSet), 0)
-
-# test_not_equal(number_of_evens(numberSet), 2)
-
-# test_between(number_of_evens(numberSet), 3, 5)
-
-# test_is_in(numberSet, 0)
-
-# test_not_in(numberSet, 3)
