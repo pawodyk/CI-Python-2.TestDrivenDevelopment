7d284e0e3d65c01aa2b32e8fbbe27df084342e2f
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-06 15:50:43 +0100

    added the functionality that allowes the choice of the coin denominations

--- vending_machine.py
+++ vending_machine.py
@@ -4,11 +4,14 @@ from byotest import *
 #  Given an amount of change that needs to be paid, we want to generate the list of coins that should be given to the customer.
 #  Our function should pay the minimum number of coins possible, and the available coin denominations are 100, 50, 20, 10, 5, 2, and 1.
 
+usd_coins = [100, 50, 25, 10, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
 
-def get_change(amount):
+
+def get_change(amount, coins=eur_coins):
     change = []
 
-    for coin in [100, 50, 20, 10, 5, 2, 1]:
+    for coin in coins:
         while coin <= amount:
             amount -= coin
             change.append(coin)
@@ -30,7 +33,11 @@ test_are_equal(get_change(100), [100])
 
 test_are_equal(get_change(3), [2, 1])
 test_are_equal(get_change(7), [5, 2])
-test_are_equal(get_change(9), [5,2,2])
+test_are_equal(get_change(9), [5, 2, 2])
+
+test_are_equal(get_change(35, usd_coins), [25, 10])
+test_are_equal(get_change(35), [20, 10, 5])
+test_not_equal(get_change(35, usd_coins), [20, 10, 5])
 
 
 print("All test pass!")
