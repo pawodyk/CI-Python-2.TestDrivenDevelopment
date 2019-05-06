71ea415272b484314111da156c8a057539f0db05
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-06 12:21:18 +0100

    completed challange 2

--- byotest.py
+++ byotest.py
@@ -1,10 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-
-
-def number_of_evens(numbers):
-    return 0
-
-
+# Testing framework functions
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
@@ -13,13 +7,49 @@ def test_not_equal(a, b):
     assert a != b, "Did not expect {0} but got {1}".format(a, b)
 
 
+def test_between(a, min, max):
+    assert a >= min and a <= max, "{0} is not in a range between {1} and {2}".format(
+        a, min, max)
+
+
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(
         collection, item)
 
 
-test_are_equal(number_of_evens(numbers), 2)
+def test_not_in(collection, item):
+    assert item not in collection, "set {0} contains {1}".format(
+        collection, item)
 
-test_not_equal(number_of_evens(numbers), 0)
 
-test_is_in(numbers, 0)
+# Test function:
+def number_of_evens(nums):
+    return sum([1 for num in nums if num % 2 == 0])
+
+
+numberSet = [1, 2, 3, 4, 5]
+
+# Testing correct execution of number_of_events function
+
+test_are_equal(number_of_evens(numberSet), 2)
+test_not_equal(number_of_evens(numberSet), 0)
+test_not_equal(number_of_evens(numberSet), 5)
+test_is_in(numberSet, 2)
+test_is_in(numberSet, 5)
+
+print("The number_of_evens function passed all tests!")
+
+
+# this tests are designed to fail, please uncomment the one you wish to execute
+
+print("\nNow, testing the testing framework")
+
+# test_are_equal(number_of_evens(numberSet), 0)
+
+# test_not_equal(number_of_evens(numberSet), 2)
+
+# test_between(number_of_evens(numberSet), 3, 5)
+
+# test_is_in(numberSet, 0)
+
+# test_not_in(numberSet, 3)
